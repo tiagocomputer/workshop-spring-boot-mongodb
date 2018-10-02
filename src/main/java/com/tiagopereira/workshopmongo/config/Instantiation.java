edb469b96ad4d5c440ea1a6cf5ef66c783c502e9
tiagopereira <tiago.pereira@mtpi.com.br>
2018-10-02 16:51:59 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/tiagopereira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/tiagopereira/workshopmongo/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.tiagopereira.workshopmongo.config;
 
 import com.tiagopereira.workshopmongo.dto.AuthorDTO;
+import com.tiagopereira.workshopmongo.dto.CommentDTO;
 import com.tiagopereira.workshopmongo.entity.Post;
 import com.tiagopereira.workshopmongo.entity.User;
 import com.tiagopereira.workshopmongo.repository.PostRepository;
@@ -41,9 +42,16 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("01/10/2018"), "Partiu viagem", "Vou viajar para São Paulo", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("02/10/2018"), "Bom dia!", "Acordei feliz hoje! :)", new AuthorDTO(maria));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("02/10/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("01/10/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("03/10/2018"), new AuthorDTO(alex));
+
+        post1.getCommentDTOS().addAll(Arrays.asList(c1, c2));
+        post2.getCommentDTOS().addAll(Arrays.asList(c3));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
-        maria.getPosts().addAll(Arrays.asList(post1,post2));
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
         userRepository.save(maria);
     }
 }
--- src/main/java/com/tiagopereira/workshopmongo/entity/Post.java
+++ src/main/java/com/tiagopereira/workshopmongo/entity/Post.java
@@ -1,11 +1,14 @@
 package com.tiagopereira.workshopmongo.entity;
 
 import com.tiagopereira.workshopmongo.dto.AuthorDTO;
+import com.tiagopereira.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -18,6 +21,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO authorDTO;
+
+    private List<CommentDTO> commentDTOS = new ArrayList<>();
 
     public Post() {
     }
@@ -68,6 +73,14 @@ public class Post implements Serializable {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public List<CommentDTO> getCommentDTOS() {
+        return commentDTOS;
+    }
+
+    public void setCommentDTOS(List<CommentDTO> commentDTOS) {
+        this.commentDTOS = commentDTOS;
     }
 
     @Override
